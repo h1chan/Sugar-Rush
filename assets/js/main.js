83d6e30bb985292f6e16192cbe1d67d3d1d9cb1e
h1chan <sanchokuanysh@gmail.com>
2026-08-21 17:22:47 +0500

    act-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,54 @@
 /*=============== SHOW & CLOSE MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
 
+/*  Show menu */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Hide menu */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MOBILE MENU ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== HOME SWIPER ===============*/
-
+const swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    grabCursor: true,
+    speed: 800,
+    effect: 'creative',
+    creativeEffect: {
+        prev: {
+            translate: ['-120%', 0, -500],
+            rotate: [0, 0, -45],
+            opacity: 0,
+        },
+        next: {
+            translate: ['120%', 0, -500],
+            rotate: [0, 0, 45],
+            opacity: 0,
+        },
+    },
+    autoplay: {
+        delay:3000,
+        disableOnIntercation: false,
+    },
+});
 
 /*=============== CHANGE HEADER STYLES ===============*/
 
