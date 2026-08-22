d478b1e761725bfc4168f098f7df0ddc2070c402
h1chan <sanchokuanysh@gmail.com>
2026-08-22 12:35:43 +0500

    finish about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,12 @@ const swiperHome = new Swiper('.home__swiper', {
 });
 
 /*=============== CHANGE HEADER STYLES ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >=50 ? header.classList.add('scroll-header')
+                      : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== PRODUCTS SWIPER ===============*/
 
