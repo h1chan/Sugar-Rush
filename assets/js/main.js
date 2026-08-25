f06fd07ae9af7582b7f3b7a124b6e5e784a023c0
h1chan <sanchokuanysh@gmail.com>
2026-08-25 17:00:50 +0500

    end home__section

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,28 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== HOME SWIPER ===============*/
-
+const swiperHome = new Swiper('.home__swiper', {
+   loop: true,
+   grabCursor: true,
+   speed: 800,
+   effect: 'creative',
+   creativeEffect: {
+      prev: {
+         translate: ['-120%', 0, -500],
+         rotate: [0, 0, -45],
+         opacity: 0,
+      },
+      next: {
+         translate: ['120%', 0, -500],
+         rotate: [0, 0, 45],
+         opacity: 0,
+      },
+   },
+   // autoplay: {
+   //    delay: 3000,
+   //    disableOnInteraction: false
+   // },
+})
 
 /*=============== CHANGE HEADER STYLES ===============*/
 
